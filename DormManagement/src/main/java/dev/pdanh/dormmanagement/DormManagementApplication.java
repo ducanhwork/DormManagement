e258f8f112a4721c6a0605116638fb014c407ccd
Duc Anh Phan <pdanh.work@gmail.com>
2024-05-21 22:12:12 +0700

    firstcommit

---
+++ DormManagement/src/main/java/dev/pdanh/dormmanagement/DormManagementApplication.java
@@ -0,0 +1,13 @@
+package dev.pdanh.dormmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DormManagementApplication.class, args);
+    }
+
+}
---
+++ DormManagement/src/test/java/dev/pdanh/dormmanagement/DormManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pdanh.dormmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
